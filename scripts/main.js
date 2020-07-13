165553a69ec9dfdd23c607f6567606acacafdc67
Rnelson24 <46324406+Rnelson24@users.noreply.github.com>
2020-07-13 12:29:48 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 let total =0;
 let tempQuestionArr = [];
 let quizArr = [];
-let answer, num, userPick;
+let answer, num, userPick, iterator= 0;
 let btnA = document.getElementById('btn1'), btnB = document.getElementById('btn2'),
-btnC = document.getElementById('btn3'), btnD = document.getElementById('btn4')
+btnC = document.getElementById('btn3'), btnD = document.getElementById('btn4'),
+nxtBtn = document.getElementById('next-btn');
 
+//Buttons event listener
 btnA.addEventListener('click',check)
 btnA.addEventListener('click', function copyAnswer(){
   userPick = btnA.innerHTML;
@@ -22,6 +24,9 @@ btnD.addEventListener('click',check)
 btnD.addEventListener('click', function copyAnswer(){
   userPick = btnD.innerHTML;
 });
+
+nxtBtn.addEventListener('click', next)
+
 //Change on screen html & css
 function morph(Qstring, a1, a2, a3, a4) {
   document.getElementById('question').innerHTML = Qstring;
@@ -30,6 +35,24 @@ function morph(Qstring, a1, a2, a3, a4) {
   document.getElementById('btn3').innerHTML = a3;
   document.getElementById('btn4').innerHTML = a4;
 }
+
+function test(){
+  alert('works');
+}
+
+let quizObject = {
+  //question:
+  //Answer:
+  //A:
+  //B:
+  //C:
+  //D:
+}
+
+let addProp = (obj, propName, propValue) => {
+  obj[propName] = propValue;
+}
+
 // random number gen
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -71,13 +94,18 @@ function shuffle(a) {
 }
 
 
-//checks for correct answers
+//checks for correct answers and qeues next question
 function check(){
   if(userPick == answer){
     total += 1;
   }
-  
+
   return;
+}
+
+function next(){
+  morph(quizArr[iterator].Question,quizArr[iterator].A,quizArr[iterator].B,quizArr[iterator].C,quizArr[iterator].D)
+  iterator +=1;
 }
 
 
@@ -87,56 +115,100 @@ function check(){
 //Number Knowledge,
 
 function numberOrder() {
+  //for (var member in quizObject) delete quizObject[member];
   let nOString;
   num = getRandomInt(1, 10);
-  tempQuestionArr.push(num + 1);
-  answer = num +1;
+  answer = num+1;
+  addProp(quizObject, "Answer",answer)
+  //alert(quizObject.Answer);
+  tempQuestionArr.push(answer);
   tempQuestionArr.push(getRandomInt(1, 10));
   tempQuestionArr.push(getRandomInt(1, 10));
   tempQuestionArr.push(getRandomInt(1, 10));
   clean(tempQuestionArr);
   shuffle(tempQuestionArr);
   //tempQuestionArray feeds possible answers
-  let ans = tempQuestionArr[0];
-  let ans2 = tempQuestionArr[1];
-  let ans3 = tempQuestionArr[2];
-  let ans4 = tempQuestionArr[3];
+  addProp(quizObject, "A",tempQuestionArr[0]);
+  addProp(quizObject, "B",tempQuestionArr[1]);
+  addProp(quizObject, "C",tempQuestionArr[2]);
+  addProp(quizObject, "D",tempQuestionArr[3]);
   tempQuestionArr = [];
 
   nOString = "What number comes" + ' ' + 'after' + ' ' + num;
+  addProp(quizObject,"Question", nOString);
+  console.log(nOString);
+  //alert(quizObject.Question);
+  //for (var member in quizObject) delete quizObject[member];
   //  alert(nOString);
   //  alert(ans);
-  morph(nOString, ans, ans2, ans3, ans4);
-  check();
-  return;
+  // morph(nOString, ans, ans2, ans3, ans4);
+  // check();
 }
 function numberOrder2(){
-  let nOString2 = 'what number comes two numbers after' + ' ' + num;
-
+  let nOString = 'what number comes two numbers after' + ' ' + num;
+  addProp(quizObject,"Question", nOString);
   tempQuestionArr.push(num + 2);
   answer = num + 2;
+  addProp(quizObject, "Answer",answer)
   tempQuestionArr.push(getRandomInt(1, 10));
   tempQuestionArr.push(getRandomInt(1, 10));
   tempQuestionArr.push(getRandomInt(1, 10));
   clean(tempQuestionArr);
   shuffle(tempQuestionArr);
   //tempQuestionArray feeds possible answers
-  let ans = tempQuestionArr[0];
-  let ans2 = tempQuestionArr[1];
-  let ans3 = tempQuestionArr[2];
-  let ans4 = tempQuestionArr[3];
+  addProp(quizObject, "A",tempQuestionArr[0]);
+  addProp(quizObject, "B",tempQuestionArr[1]);
+  addProp(quizObject, "C",tempQuestionArr[2]);
+  addProp(quizObject, "D",tempQuestionArr[3]);
   tempQuestionArr = [];
-
-  morph(nOString2, ans, ans2, ans3, ans4);
-  check();
+  quizArr.push(quizObject);
+  //for (var member in quizObject) delete quizObject[member];
+  // check();
 }
 
-function formQuiz(){
-  quizArr.push(numberOrder());
-  quizArr.push(numberOrder2());
-}
-function runQuiz(){
 
+function smallerOrLarger(){
+  let bigOrSmall = getRandomInt(1,2);
+  num = getRandomInt(1, 10);
+  tempQuestionArr.push(getRandomInt(num, 10));
+  tempQuestionArr.push(getRandomInt(1, num);
+  clean(tempQuestionArr);
+  shuffle(tempQuestionArr);
+  if (bigOrSmall == 1){
+    let nOString = 'which number is bigger?';
+    for(let i = 0; i< tempQuestionArr.length; i++){
+      if(tempQuestionArr[i]>num){
+        answer = tempQuestionArr[i];
+      }
+    }
+
+  }else{
+    nOString = 'which number is smaller?'
+    for(let i = 0; i< tempQuestionArr.length; i++){
+      if(tempQuestionArr[i]<num){
+        answer = tempQuestionArr[i];
+  }
 }
 
-formQuiz();
+//Visual Arrays function needs to be implemented
+
+
+
+
+
+function compileQuiz(){
+  for(var i = 0; i<4; i++){
+    numberOrder2();
+    quizArr.push(quizObject);
+    numberOrder();
+    quizArr.push(quizObject);
+  }
+  //alert(quizArr.length);
+  for(let i = 0; i<quizArr.length; i++){
+    console.log(quizArr[i].Question);
+    console.log(quizArr[i].A);
+
+  }
+}
+
+compileQuiz();
